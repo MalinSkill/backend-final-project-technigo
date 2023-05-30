be3cca72292a72388ac1ce2c6cc55f36ddcf0bba
Malin <malin@MacBook-Pro-4.local>
2023-05-30 10:21:07 +0200

    set up dependencies

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -13,7 +20,7 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Let's start this final project!");
 });
 
 // Start the server
